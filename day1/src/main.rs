85c6e82c2e28d323bed52acb0474ce6022f96d2f
xct <xct@vulndev.io>
2022-11-30 23:13:57 -0800

    initial

---
+++ day1/src/main.rs
@@ -0,0 +1,58 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+fn part1() {
+    // input: number of calories each elf is carrying, per item, separated by blank line
+    // result: how many calories on elf with most calories  
+    let mut max = 0;
+    let mut cur = 0;
+    if let Ok(lines) = read_lines("./input.txt") {
+        for line in lines {
+            if let Ok(this) = line {
+                if this.len() == 0 {
+                    if cur > max {
+                        max = cur;
+                    }
+                    cur = 0;
+                    continue;
+                }
+                let value = this.parse::<i32>().unwrap();
+                cur += value;                
+            }
+        }
+    }
+    println!("Max:  {}", max);
+}
+
+fn part2() {
+    // top 3 added together instead of max
+    let mut totals = Vec::new();
+    let mut cur = 0;
+    if let Ok(lines) = read_lines("./input.txt") {
+        for line in lines {
+            if let Ok(this) = line {
+                if this.len() == 0 {
+                    totals.push(cur);
+                    cur = 0;                    
+                    continue;
+                }
+                let value = this.parse::<i32>().unwrap();
+                cur += value;                
+            }
+        }
+    }
+    totals.sort_by(|a, b| b.cmp(a));
+    println!("Top3: {}", totals[0]+totals[1]+totals[2]);
+}
+
+fn main() {
+    part1(); // 64929
+    part2(); // 193697
+}
